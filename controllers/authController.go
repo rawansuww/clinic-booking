3329f0f5acdd3dc14a4255aa9a8b485c67807ed4
unicornlab-shehanaza <frilly3301@outlook.com>
2022-03-16 10:46:36 +0400

    commit

--- controllers/authController.go
+++ controllers/authController.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"regexp"
 
 	"github.com/gin-gonic/gin"
@@ -10,98 +11,8 @@ import (
 	"github.com/rawansuww/clinic-booking/middleware"
 	"github.com/rawansuww/clinic-booking/models"
 	"golang.org/x/crypto/bcrypt"
-	_ "golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
-	_ "gorm.io/gorm"
 )
-
-// Signup creates a user in db
-func Signup(c *gin.Context) {
-	var input SignUp
-
-	err := c.ShouldBindJSON(&input)
-
-	fmt.Println(string(input.Role))
-
-	if err != nil {
-		fmt.Println(err)
-
-		c.JSON(400, gin.H{
-			"msg": "Invalid JSON or missing field!",
-		})
-		c.Abort()
-
-		return
-	}
-
-	hashed, err := bcrypt.GenerateFromPassword([]byte(input.Password), 8)
-	if input.Role == "doctor" {
-		doc := models.Doctor{Name: input.Name, Email: input.Email, Password: string(hashed), Role: input.Role}
-
-		m, err := regexp.MatchString("@cxunicorn.com", input.Email)
-		if err != nil {
-			fmt.Println("your regex is faulty")
-			// you should log it or throw an error
-
-		}
-		if !m {
-			c.JSON(400, "Your email should include @cxunicorn.com")
-			return
-		}
-
-		if models.DB.Create(&doc).Error != nil {
-			c.JSON(400, "Your email already exists in this system!")
-			return
-		}
-
-		c.JSON(200, doc)
-	}
-	if input.Role == "patient" {
-		patient := models.Patient{Name: input.Name, Email: input.Email, Password: string(hashed), Role: input.Role}
-		err := models.DB.Create(&patient)
-
-		if err != nil {
-			c.JSON(400, "Your email already exists in this system!")
-			return
-		}
-		c.JSON(200, patient)
-	}
-	if input.Role == "admin" {
-		admin := models.Admin{Name: input.Name, Email: input.Email, Password: string(hashed), Role: input.Role}
-		m, err := regexp.MatchString("@cxunicorn.com", input.Email)
-		if err != nil {
-			fmt.Println("your regex is faulty")
-			// you should log it or throw an error
-
-		}
-		if !m {
-			c.JSON(400, "Your email should include @cxunicorn.com")
-			return
-		}
-
-		err2 := models.DB.Create(&admin)
-		if err2 != nil {
-			c.JSON(400, "Your email already exists in this system!")
-			return
-		}
-
-		c.JSON(200, admin)
-	}
-
-	if err != nil {
-		log.Println(err.Error())
-
-		c.JSON(500, gin.H{
-			"msg": "error hashing password",
-		})
-		c.Abort()
-
-		return
-	}
-
-}
-
-// controllers/public.go
 
 // LoginPayload login body
 type LoginPayload struct {
@@ -123,34 +34,96 @@ type SignUp struct {
 	Name     string `json:"name" binding:"required"`
 }
 
+// Signup creates a user in db
+func Signup(c *gin.Context) {
+	var input SignUp
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON or missing field!"})
+		c.Abort()
+		return
+	}
+
+	hashed, err := bcrypt.GenerateFromPassword([]byte(input.Password), 8)
+	if input.Role == "doctor" {
+		doc := models.Doctor{Name: input.Name, Email: input.Email, Password: string(hashed), Role: input.Role}
+		m, err := regexp.MatchString("@cxunicorn.com", input.Email) //ALL DOCS AND ADMINS GET REGEX
+		if err != nil {
+			fmt.Println("your regex is faulty")
+			// you should log it or throw an error
+		}
+		if !m {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Your email should include @cxunicorn.com"})
+			return
+		}
+
+		if models.DB.Create(&doc).Error != nil { //throw error message on
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Your email already exists in this system!"})
+			return
+		}
+
+		c.JSON(http.StatusOK, doc)
+	}
+	if input.Role == "patient" {
+		patient := models.Patient{Name: input.Name, Email: input.Email, Password: string(hashed), Role: input.Role}
+		err := models.DB.Create(&patient)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Your email already exists in this system!"})
+			return
+		}
+		c.JSON(http.StatusOK, patient)
+	}
+	if input.Role == "admin" {
+		admin := models.Admin{Name: input.Name, Email: input.Email, Password: string(hashed), Role: input.Role}
+		m, err := regexp.MatchString("@cxunicorn.com", input.Email)
+		if err != nil {
+			//fault regex
+		}
+		if !m {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Your email should include @cxunicorn.com"})
+			return
+		}
+
+		err2 := models.DB.Create(&admin)
+		if err2 != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Your email already exists in this system!"})
+			return
+		}
+
+		c.JSON(http.StatusOK, admin)
+	}
+
+	if err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error hashing password"})
+		c.Abort()
+		return
+	}
+
+}
+
 // Login logs users in
-//THE LOGIN I NEED TO DECIDE HOW TO LOGIN AS DIFFERENT TYPES OF USERS....
 func Login(c *gin.Context) {
 	var payload LoginPayload
-	//var user models.Doctor
-
 	var user models.Doctor
 	var doc models.Doctor
 	var patient models.Patient
 	var admin models.Admin
 
-	//var user LoginPayload
-
 	err := c.ShouldBindJSON(&payload)
 	if err != nil {
-		c.JSON(400, gin.H{
-			"msg": "One of the fields is missing. Invalid JSON",
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "One of the fields is missing. Invalid JSON",
 		})
 		c.Abort()
 		return
 	}
 	if payload.Role == "doctor" {
-		fmt.Println("enter")
-
 		result := models.DB.Where("email = ?", payload.Email).Find(&(doc))
 		if result.Error == gorm.ErrRecordNotFound {
-			c.JSON(401, gin.H{
-				"msg": "invalid user credentials",
+			c.JSON(400, gin.H{
+				"error": "invalid user credentials",
 			})
 			c.Abort()
 			return
@@ -163,7 +136,7 @@ func Login(c *gin.Context) {
 		result := models.DB.Where("email = ?", payload.Email).Find(&patient)
 		if result.Error == gorm.ErrRecordNotFound {
 			c.JSON(401, gin.H{
-				"msg": "invalid user credentials",
+				"error": "invalid user credentials",
 			})
 			c.Abort()
 			return
@@ -175,7 +148,7 @@ func Login(c *gin.Context) {
 		result := models.DB.Where("email = ?", payload.Email).Find(&admin)
 		if result.Error == gorm.ErrRecordNotFound {
 			c.JSON(401, gin.H{
-				"msg": "invalid user credentials",
+				"error": "invalid user credentials",
 			})
 			c.Abort()
 			return
@@ -184,14 +157,10 @@ func Login(c *gin.Context) {
 
 	}
 
-	fmt.Println(user)
-	//	result := models.DB.Where("email = ?", payload.Email).Find(&user)
-
 	e := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(payload.Password))
 	if e != nil {
-
 		c.JSON(401, gin.H{
-			"msg": "Wrong password",
+			"error": "Wrong password",
 		})
 		c.Abort()
 		return
@@ -215,9 +184,9 @@ func Login(c *gin.Context) {
 
 	tokenResponse := LoginResponse{
 		Token:   signedToken,
-		Message: "Login Successful",
+		Message: "Login Successful!",
 	}
 
-	c.JSON(200, tokenResponse)
+	c.JSON(http.StatusOK, tokenResponse)
 
 }
